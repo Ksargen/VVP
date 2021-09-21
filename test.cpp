2669ccd6e95daa443c68b607303bffb74ea6d410
Egor Basharov <88620780+Ksargen@users.noreply.github.com>
2021-09-21 17:09:54 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{   
+    int a;
+    int b;
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите стороны прямоугольника:";
+    cin >> a >> b;
+    cout << "Площадь прямоугольника: " << a * b;
+    cout << "\nПериметр прямоуглольника:" << 2 * (a + b);
+    return 0;
+}
