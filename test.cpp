3fd2a33c813ba0ec044689a0eebbf110ae965101
Egor Basharov <88620780+Ksargen@users.noreply.github.com>
2021-09-21 17:13:51 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{   
+    float Pi = 3.14;
+    int d;
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите диаметр окружности:";
+    cin >> d;
+    cout << "Площадь окружности: " << Pi * d;
+    return 0;
+}
