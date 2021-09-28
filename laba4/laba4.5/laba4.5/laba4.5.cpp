d31dd9d81946fb1c0ff77e3e85927f5d5bd38ca0
Egor Basharov <88620780+Ksargen@users.noreply.github.com>
2021-09-28 14:02:48 +0000

    Add files via upload

---
+++ laba4/laba4.5/laba4.5/laba4.5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите 2 ненулевых числа:";
+	int a;
+	int b;
+	cin >> a >> b;
+	cout << "Сумма их модулей " << abs(a) + abs(b);
+	cout << "\nРазность их модулей: " << abs(a) - abs(b);
+	cout << "\nПроизведение их модулей: " << abs(a) * abs(b);
+	cout << "\nЧастное первого модуля от второго: " << abs(a) / abs(b);
+	return 0;
+}
