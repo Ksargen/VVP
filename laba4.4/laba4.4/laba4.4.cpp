90feb5514a434fafe8343395864c7057732be17a
Egor Basharov <88620780+Ksargen@users.noreply.github.com>
2021-09-28 13:55:52 +0000

    Add files via upload

---
+++ laba4.4/laba4.4/laba4.4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+#include <locale.h>
+
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите 2 ненулевых числа:";
+	int a;
+	int b;
+	cin >> a >> b;
+	cout << "Сумма их квадратов: " << pow(a, 2) + pow(b, 2);
+	cout << "\nРазность их квадратов: " << pow(a, 2) - pow(b, 2);
+	cout << "\nПроизведение их квадратов: " << pow(a, 2) * pow(b, 2);
+	cout << "\nЧастное первого квадрата от второго: " << pow(a, 2) / pow(b, 2);
+	return 0;
+}
