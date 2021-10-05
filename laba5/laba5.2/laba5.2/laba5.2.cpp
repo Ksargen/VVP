eb5391263f60670ab6a4dff4fb78b06687a04c15
Egor Basharov <88620780+Ksargen@users.noreply.github.com>
2021-10-05 16:48:02 +0300

    Add files via upload

---
+++ laba5/laba5.2/laba5.2/laba5.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <pch.h>
+#include <iostream>
+#include <math.h>
+#include <locale.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+int main()
+{
+	int A;
+	int B;
+	int C;
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите три координаты: ";
+	cin >> A >> B >> C;
+	double N = std::fmin(A, std::fmin(B, C));
+	int A1 = A - N;
+	int B1 = B - N;
+	int C1 = C - N;
+	if (A1 == 0) {
+		cout << "\nДлина АС: " << C1;
+		cout << "\nДлина BС: " << abs(C1 - B1);
+		cout << "\nСумма длин АС и ВС: " << C1 + abs(C1 - B1);
+	}
+	if (B1 == 0) {
+		cout << "\nДлина АС: " << abs(A1 - C1);
+		cout << "\nДлина BС: " << C1;
+		cout << "\nСумма длин АС и ВС: " << C1 + abs(A1 - C1);
+	}
+	if (C1 == 0) {
+		cout << "\nДлина АС: " << A1;
+		cout << "\nДлина BС: " << abs(B1 - C1);
+		cout << "\nСумма длин АС и ВС: " << A1 + abs(B1 - C1);
+	}
+	return 0;
+}
