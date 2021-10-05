a582949a3ebc752c40ec59736a5d1988ce092e05
Egor Basharov <88620780+Ksargen@users.noreply.github.com>
2021-10-05 17:03:23 +0300

    Add files via upload

---
+++ laba5/laba5.4/laba5.4/laba5.4.cpp
@@ -0,0 +1,23 @@
+﻿// laba5.4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include <pch.h>
+#include <iostream>
+#include <math.h>
+#include <locale.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+int main()
+{
+	int x1, x2, y1, y2;
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите координаты первого угла: ";
+	cin >> x1 >> y1;
+	cout << "Введите координаты второго угла угла: ";
+	cin >> x2 >> y2;
+	int x = abs(x1 - x2);
+	int y = abs(y1 - y2);
+	cout << "Площадь прямоугольника: " << x * y << "\nПериметр данного прямоугольика: " << 2 * (x + y);
+	return 0;
+}
