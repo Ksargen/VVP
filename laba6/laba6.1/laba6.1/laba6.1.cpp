6546b4da47a6ed79446fbf5949be7f3b1e20fa2f
Egor Basharov <88620780+Ksargen@users.noreply.github.com>
2021-10-12 16:46:37 +0300

    Add files via upload

---
+++ laba6/laba6.1/laba6.1/laba6.1.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y, buff;
+	cin >> x >> y;
+	buff = x;
+	x = y;
+	y = buff;
+
+    cout << x << y; 
+}
---
+++ laba6/laba6.2/laba6.2/laba6.2.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, buff;
+	cin >> a >> b >> c;
+	buff = a;
+	a = c;
+	c = b;
+	b = buff;
+	cout << a << b << c; 
+}
