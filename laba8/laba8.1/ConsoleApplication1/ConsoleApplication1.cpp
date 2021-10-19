ec01d30a00f6f3a7dd7f4ecc3b97b2b8b6a59e3d
Egor Basharov <88620780+Ksargen@users.noreply.github.com>
2021-10-19 17:44:04 +0300

    Add files via upload

---
+++ laba8/laba8.1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите размер файла в байтах: ";
+    int a;
+    cin >> a;
+    if (a % 1024 > 0) {
+    cout << "Файл занимает " << a / 1024 << " целых килобайт.";
+    }
+    
+}
---
+++ laba8/laba8.2/laba8.2/laba8.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a, b;
+    cout << "Введите длину отрезка А, а затем длину Б: ";
+    cin >> a >> b;
+    int c;
+    c = a / b;
+    cout << "В отрезок А вмещается " << c << " отрезков В.";
+}
