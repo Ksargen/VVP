d172a90914f765538d87d476a48fb2c549658612
Egor Basharov <ksargen.k@yandex.ru>
2021-10-25 02:01:40 +0300

    laba9.5 fix 2

--- laba9/laba9.5/main.cpp
+++ laba9/laba9.5/main.cpp
@@ -9,6 +9,14 @@ int main()
     cout << "¬видете год: ";
     int k;
     cin >> k;
+    if (k == 0){
+        cout << "нормально пиши https://ru.wikipedia.org/wiki/0_год";
+        return 0;
+    }
+    if (k < 0){
+        cout << "Ёто " << (k + 1) / 100 - 1 <<" век.";
+        return 0;
+    }
     cout << "Ёто " << (k - 1) / 100 + 1 <<" век.";
     return 0;
 }
