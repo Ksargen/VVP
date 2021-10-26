2e4fb6c52eadbd45b5eaf6f071b2a06a0c2b2fe0
Egor Basharov <88620780+Ksargen@users.noreply.github.com>
2021-10-26 17:22:09 +0300

    Add files via upload

---
+++ laba11/laba11.4/laba10.4/laba10.4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+#include <locale.>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите координаты точки A: ";
+	int Ax, Ay;
+	cin >> Ax >> Ay;
+	if ((Ax > 0) and (Ay > 0)) {
+		cout << "Точка в I четверти.";
+	}
+	else if ((Ax < 0) and (Ay > 0)) {
+		cout << "Точка в II четверти.";
+	}
+	else if ((Ax < 0) and (Ay < 0)) {
+		cout << "Точка в III четверти.";
+	}
+	else {
+		cout << "Точка в IV четверти.";
+	}
+}
